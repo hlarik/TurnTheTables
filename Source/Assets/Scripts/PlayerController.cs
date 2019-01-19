bc970d4de55cafebc099098e895356db61965cfc
aysesunar <30694850+aysesunar@users.noreply.github.com>
2019-01-19 13:24:52 +0300

    camera control added aldo fixed some animations

--- Source/Assets/Scripts/PlayerController.cs
+++ Source/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,54 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float walkSpeed = 2;
+    public float runSpeed = 6;
+
+    public float turnSmoothTime = 0.2f;
+    float turnSmoothVelocity;
+
+    public float speedSmoothTime = 0.1f;
+    float speedSmoothVelocity;
+    float currentSpeed;
+
+    Animator animator;
+    public Camera mainCamera;
+    Transform cameraT;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        cameraT = mainCamera.transform;
+    }
+
+    void Update()
+    {
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+        float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
+        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+
+    }
+}
+
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -56,13 +106,13 @@ public class PlayerController : MonoBehaviour {
             }
         }
 
-        var z = Input.GetAxis("Vertical") * speed;
-        var y = Input.GetAxis("Horizontal") * rotationSpeed;
+        //var z = Input.GetAxis("Vertical") * speed;
+        //var y = Input.GetAxis("Horizontal") * rotationSpeed;
 
-        transform.Translate( 0, 0, z);
-        transform.Rotate(0, y, 0);
+        //transform.Translate( 0, 0, z);
+        //transform.Rotate(0, y, 0);
 
-        /*if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             //rb.AddForce(-300*speed, 0, 0);
             transform.Translate(-speed, 0, 0);
@@ -81,7 +131,7 @@ public class PlayerController : MonoBehaviour {
         {
             //rb.AddForce(0, 0, -300*speed);
             transform.Translate(0, 0, -speed);
-        }*/
+        }
 
         if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
@@ -163,4 +213,4 @@ public class PlayerController : MonoBehaviour {
     {
         isGrounded = true;
     }
-}
+}*/
