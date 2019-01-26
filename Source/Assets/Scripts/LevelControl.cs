988088ef2ee72ee011e26dc940bc16aca5b06cb2
Hareem Larik <32205943+hlarik@users.noreply.github.com>
2019-01-27 01:05:16 +0300

    scene transition added

---
+++ Source/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+    public string sceneName;
+    public GameObject uiObject;
+    bool collision;
+
+    void Start()
+    {
+        collision = false;
+        uiObject.SetActive(false); 
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && collision )
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") )
+        {
+            collision = true;
+            uiObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        collision = false;
+        uiObject.SetActive(false);
+    }
+}
