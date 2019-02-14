bbf057db5e4c914056488a18dd435d034da02c89
aysesunar <30694850+aysesunar@users.noreply.github.com>
2019-02-14 16:32:14 +0300

    camera passing through walls disabled

--- Source/Assets/Scripts/CameraController.cs
+++ Source/Assets/Scripts/CameraController.cs
@@ -14,8 +14,13 @@ public class CameraController : MonoBehaviour
     Vector3 rotationSmoothVelocity;
     Vector3 currentRotation;
 
+    bool cameraTurn = true;
+
     float yaw;
     float pitch;
+
+    Vector3 center = new Vector3(0f, 0.5f, 0f);
+    RaycastHit hit;
 
     void Start()
     {
@@ -29,15 +34,42 @@ public class CameraController : MonoBehaviour
 
     void LateUpdate()
     {
-        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
-        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
-        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+        if (cameraTurn) {
+            yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+            pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+            pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
 
-        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
-        transform.eulerAngles = currentRotation;
+            currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+            transform.eulerAngles = currentRotation;
+        }
 
-        transform.position = target.position - transform.forward * dstFromTarget;
+        Vector3 wantedCameraPosition = target.position - transform.forward * dstFromTarget;
 
+        // Raycasting 
+        Vector3 rayDirection = (wantedCameraPosition - (target.position + center)).normalized;
+
+        if (Physics.Raycast(target.position + center, rayDirection, out hit, dstFromTarget)
+            && hit.transform != target.parent) // ignore ray-casts that hit the user. DR
+        {
+            // Debug.Log(hit.transform.name  + " " + direction.ToString());
+            wantedCameraPosition.x = hit.point.x;
+            wantedCameraPosition.z = hit.point.z;
+            //wantedPosition.y = wantedPosition.y);
+        }
+
+        transform.position = wantedCameraPosition;
+
+    }
+
+    //To disable camera Turn in cutscenes and dialogues
+    public void diableCameraMouse()
+    {
+        cameraTurn = false;
+    }
+
+    public void enableCameraMouse()
+    {
+        cameraTurn = true;
     }
 
 }
