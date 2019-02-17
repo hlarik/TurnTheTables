acfe900deeedd972040ae9fb576c9f15275ed8c3
aysesunar <30694850+aysesunar@users.noreply.github.com>
2019-02-17 03:54:13 +0300

    merged changes manually

---
+++ Source/Assets/Scripts/FirstScenarioTrigger.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class FirstScenarioTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public PlayableDirector pd;
+    //public TimelineAsset timeline;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            pd.Play();
+            Destroy(this);
+        }
+        
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //timeline = GetComponent<TimelineAsset>();
+        pd = GetComponent<PlayableDirector>();
+    }
+
+}
---
+++ Source/Assets/Scripts/MotherAnimations.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherAnimations : MonoBehaviour
+{
+    Animator anim;
+    public GameObject mainPlayer;
+    public void Talk1()
+    {
+        GetComponent<Animator>().Play("Talking1", -1, 0f);
+    }
+
+    public void TalkMain()
+    {
+        // mainPlayer = GetComponent<GameObject>();
+        mainPlayer.GetComponent<Animator>().Play("Shrugging", -1, 0f);
+
+    }
+
+    //
+    /*  public void Talk2()
+      {
+          anim.Play("New cube Animation");
+      }*/
+}
