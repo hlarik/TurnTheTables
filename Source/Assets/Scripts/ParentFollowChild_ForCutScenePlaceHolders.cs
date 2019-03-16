82c04b329e36407ad18416c1c2466faf7515d4a7
aysesunar <30694850+aysesunar@users.noreply.github.com>
2019-03-16 03:15:25 +0300

    first cutscene finalized (animation placeholders added)

---
+++ Source/Assets/Scripts/ParentFollowChild_ForCutScenePlaceHolders.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentFollowChild_ForCutScenePlaceHolders : MonoBehaviour
+{
+    GameObject child;
+
+    void Start()
+    {
+        child = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = child.transform.position;
+        this.transform.rotation = child.transform.rotation;
+    }
+}
