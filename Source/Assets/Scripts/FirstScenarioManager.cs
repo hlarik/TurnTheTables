6b3a8e1dff04e4e5d8a2f9d9a82ac9affe829cc2
Hareem Larik <32205943+hlarik@users.noreply.github.com>
2019-04-29 02:35:44 +0300

    Outdoor scene updated

--- Source/Assets/Scripts/FirstScenarioManager.cs
+++ Source/Assets/Scripts/FirstScenarioManager.cs
@@ -12,11 +12,20 @@ public class FirstScenarioManager : MonoBehaviour
     public GameObject mother;
     public GameObject virtualCam;
     public GameObject timeline;
+    public GameObject[] moveSpots;
     PlayableDirector pd;
+    PlayerController playerControllerScript;
+    GameObject MotherParent;
+    Vector3 turnSmoothVelocity;
+    float turnSmoothTime = 0.2f;
+    float rotSpeed = 8f;
+    bool sceneEnded = false;
+    bool turningTowardsTarget = false;
+    bool reachedTarget = false;
+    int curMS = 0;
 
     private GameObject cameraScript;
     
-    PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +33,50 @@ public class FirstScenarioManager : MonoBehaviour
         pd = timeline.GetComponent<PlayableDirector>();
         playerControllerScript = GameObject.Find("Violet").GetComponent<PlayerController>();
         cameraScript = Camera.main.gameObject;
+        MotherParent = GameObject.Find("Mother_Parent");
     }
 
-    public void OnTriggerEnter(Collider other)
+    void Update()
+    {
+        //if scene has ended mom goes back to her Batcave
+        if (sceneEnded)
+        {
+            // turn towards target
+            if (turningTowardsTarget)
+            {
+                Vector3 direction = moveSpots[curMS].transform.position - MotherParent.transform.position;
+                MotherParent.transform.rotation = Quaternion.Slerp(MotherParent.transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime * 2);
+                if ((int)Quaternion.LookRotation(direction).eulerAngles.y == (int)MotherParent.transform.rotation.eulerAngles.y)
+                {
+                    turningTowardsTarget = false;
+                }
+            }
+            else
+            {
+                mother.GetComponent<Animator>().SetTrigger("walk");
+                float step = playerControllerScript.walkSpeed * Time.deltaTime / 2;
+                MotherParent.transform.position = Vector3.MoveTowards(MotherParent.transform.position, moveSpots[curMS].transform.position, step);
+
+                if (Vector3.Distance(moveSpots[curMS].transform.position, MotherParent.transform.position) < 0.1f)
+                {
+                    turningTowardsTarget = true;
+                    curMS++;
+                    if (curMS >= moveSpots.Length)
+                    {
+                        sceneEnded = false;
+                        reachedTarget = true;
+                    }
+                }
+            }
+        }
+        if (reachedTarget)
+        {
+            //disappear into thin air *poof*
+            MotherParent.SetActive(false);
+        }
+    }
+
+        public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
@@ -39,10 +89,8 @@ public class FirstScenarioManager : MonoBehaviour
 
     public void TalkWithNPC()
     {
-        mother.GetComponent<InteractWithCharacter>().Interact();
+        mother.gameObject.GetComponent<InteractWithCharacter>().Interact();
         maincharacter.GetComponent<Animator>().SetFloat("speedPercent", 0.0f);
-        VD.SetNode(0);
-        Debug.Log("in talk");
     }
 
     public void EndScenario()
@@ -52,8 +100,7 @@ public class FirstScenarioManager : MonoBehaviour
         cameraScript.GetComponent<CameraController>().enableCameraMouse();
 
         virtualCam.SetActive(false);
-        pd = null;
-        //Destroy(this);
+        sceneEnded = true;
+        turningTowardsTarget = true;
     }
-
 }
