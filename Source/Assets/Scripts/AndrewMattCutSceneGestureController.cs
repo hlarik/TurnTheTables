a2c7d78162692e895191978f6dae21e90e8cbca6
aysesunar <30694850+aysesunar@users.noreply.github.com>
2019-05-03 12:54:02 +0300

    Music added to final battle and animations added to andrew-Matt cutscene

---
+++ Source/Assets/Scripts/AndrewMattCutSceneGestureController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndrewMattCutSceneGestureController : MonoBehaviour
+{
+    Animator andrewAnim;
+    Animator violetAnim;
+    
+    void Start()
+    {
+        andrewAnim = GameObject.Find("Andrew").GetComponent<Animator>();
+        violetAnim = GameObject.Find("Violet").GetComponent<Animator>();
+    }
+    
+    public void MakeAndrewSad()
+    {
+        andrewAnim.SetTrigger("isSadTalking");
+    }
+
+    public void MakeAndrewGrateful()
+    {
+        andrewAnim.SetTrigger("isGrateful");
+    }
+
+    public void MakeAndrewSadIdle()
+    {
+        andrewAnim.SetTrigger("isSadIdle");
+    }
+
+    public void MakeAndrewTalk()
+    {
+        andrewAnim.SetTrigger("isTalking");
+    }
+
+    public void MakeVioletTalk()
+    {
+        violetAnim.SetTrigger("talking");
+    }
+
+    public void MakeVioletHeadGesture()
+    {
+        violetAnim.SetTrigger("HeadGesture");
+    }
+
+    public void MakeVioletHappyHandGesture()
+    {
+        violetAnim.SetTrigger("HappyHandGesture");
+    }
+}
