033786c624a764786ad42539abad2d9f3da3c265
Hareem Larik <32205943+hlarik@users.noreply.github.com>
2019-05-07 17:12:47 +0300

    Violet Sliding fixed

--- Source/Assets/Scripts/ChairTrigger.cs
+++ Source/Assets/Scripts/ChairTrigger.cs
@@ -7,20 +7,29 @@ using UnityEngine.UI;
 
 public class ChairTrigger : MonoBehaviour
 {
-    public Transform maincharacter;
+    public GameObject violetChair;
+    public GameObject Violet;
     public GameObject uiObject;
     public GameObject cutScene;
     public GameObject LevelCompleteCanvas;
     public GameObject TaskCanvas;
-    
+    public GameObject[] moveSpots;
+
     private GameObject cameraScript;
 
     Animator anim;
     PlayableDirector pd;
     TaskManager taskManagerScript;
-
+    PlayerController playerControllerScript;
+    Vector3 turnSmoothVelocity;
+    float turnSmoothTime = 0.2f;
+    float rotSpeed = 8f;
     bool hasPlayed = false;
     bool collision;
+    bool goToChair = false;
+    bool turningTowardsTarget = false;
+    bool reachedTarget = false;
+    int curMS = 0;
 
     void Start()
     {
@@ -31,6 +40,7 @@ public class ChairTrigger : MonoBehaviour
         anim = GetComponent<Animator>();
         pd = cutScene.GetComponent<PlayableDirector>();
         taskManagerScript = GameObject.Find("TaskManager").GetComponent<TaskManager>();
+        playerControllerScript = GameObject.Find("Violet").GetComponent<PlayerController>();
     }
 
     void Update()
@@ -42,14 +52,16 @@ public class ChairTrigger : MonoBehaviour
             GameObject.Find("Violet").GetComponent<PlayerController>().enabled = false;
             cameraScript.GetComponent<CameraController>().disableCameraMouse();
             //state = "isStanding";
-            if (pd != null)
+            /*if (pd != null)
             {
                 pd.Play();
                 hasPlayed = true;
-            }
+            }*/
+            goToChair = true;
+            turningTowardsTarget = true;
         }
 
-        if (pd != null && hasPlayed)
+        /*if (pd != null && hasPlayed)
         {
             if (pd.state != PlayState.Playing)
             {
@@ -59,6 +71,62 @@ public class ChairTrigger : MonoBehaviour
                 pd = null;
                 GameObject.Find("Violet").GetComponent<PlayerController>().enabled = true;
                 cameraScript.GetComponent<CameraController>().enableCameraMouse();
+            }
+        }*/
+
+        if (goToChair)
+        {
+            // turn towards target
+            if (turningTowardsTarget)
+            {
+                Vector3 direction = moveSpots[curMS].transform.position - Violet.transform.position;
+                Violet.transform.rotation = Quaternion.Slerp(Violet.transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime * 2);
+                if ((int)Quaternion.LookRotation(direction).eulerAngles.y == (int)Violet.transform.rotation.eulerAngles.y)
+                {
+                    turningTowardsTarget = false;
+                    GetComponent<Animator>().SetTrigger("atTarget");
+                }
+            }
+            else
+            {
+                Violet.GetComponent<Animator>().SetFloat("speedPercent", 0.5f);
+                float step = playerControllerScript.walkSpeed * Time.deltaTime / 2;
+                Violet.transform.position = Vector3.MoveTowards(Violet.transform.position, moveSpots[curMS].transform.position, step);
+
+                if (Vector3.Distance(moveSpots[curMS].transform.position, Violet.transform.position) < 0.1f)
+                {
+                    turningTowardsTarget = true;
+                    curMS++;
+                    if (curMS >= moveSpots.Length - 1)
+                    {
+                        goToChair = false;
+                        reachedTarget = true;
+                    }
+                }
+            }          
+        }
+
+        if (reachedTarget)
+        {
+            if (turningTowardsTarget)
+            {
+                Vector3 direction = moveSpots[curMS].transform.position - Violet.transform.position;
+                Violet.transform.rotation = Quaternion.Slerp(Violet.transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime * 2);
+                if ((int)Quaternion.LookRotation(direction).eulerAngles.y == (int)Violet.transform.rotation.eulerAngles.y)
+                {
+                    turningTowardsTarget = false;
+                }
+            }
+            else
+            {
+                GetComponent<Animator>().SetTrigger("violetSit");
+                Violet.GetComponent<Animator>().SetBool("atTarget", true); // <-- bunu yap
+                
+                //Time.timeScale = 0;
+                //GameObject.Find("Violet").GetComponent<PlayerController>().enabled = true;
+                //cameraScript.GetComponent<CameraController>().enableCameraMouse();
+                reachedTarget = false;
+                LevelCompleteCanvas.SetActive(true);
             }
         }
     }
@@ -92,8 +160,11 @@ public class ChairTrigger : MonoBehaviour
     {
         Time.timeScale = 1;
         TaskCanvas.SetActive(false);
-       // GameObject.Find("Violet").GetComponent<PlayerController>().enabled = true;
-       // cameraScript.GetComponent<CameraController>().enableCameraMouse();
+        Violet.GetComponent<Animator>().SetBool("atTarget", false);
+        GetComponent<Animator>().SetTrigger("atTarget");
+        GameObject.Find("Violet").GetComponent<PlayerController>().enabled = true;
+        cameraScript.GetComponent<CameraController>().enableCameraMouse();
+
         Destroy(this);
     }
 
